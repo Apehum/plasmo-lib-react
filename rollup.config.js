eadc0d2882afba206ea631124fa9a77865af31c8
Apehum <apehumchik@gmail.com>
2021-08-24 17:16:49 +0800

    update to npm16

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
 
 import pkg from "./package.json";
@@ -37,16 +37,7 @@ export default {
             clean: true
         }),
         commonjs({
-            include: ["node_modules/**"],
-            namedExports: {
-                "node_modules/react/react.js": [
-                    "Children",
-                    "Component",
-                    "PropTypes",
-                    "createElement"
-                ],
-                "node_modules/react-dom/index.js": ["render"]
-            }
+            include: ["node_modules/**"]
         })
     ]
 };
